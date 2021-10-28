e9340abe90e091492a464e1a982d5d97a1450343
Takumi <nase49946@gmail.com>
2021-10-28 22:10:14 +0900

    記事一覧取得 #1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,33 @@
-const Home = () => {
+import { client } from '../libs/client';
+
+const Home = (props: any) => {
+  const { articleList } = props;
+
   return (
-    <>
-      <h1>デモ</h1>
-    </>
+    <ul>
+      {articleList.map((article: any) => (
+        <li key={article.id}>
+          <p>{article.title}</p>
+          <p>{article.category.name}</p>
+          <div>
+            {article.tags.map((tag: any) => (
+              <span key={tag.id}>{tag.name}</span>
+            ))}
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 };
 
 export default Home;
+
+export const getStaticProps = async () => {
+  const blog = await client.get({ endpoint: 'blog' });
+
+  return {
+    props: {
+      articleList: blog.contents,
+    },
+  };
+};
