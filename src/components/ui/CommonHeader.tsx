e909b9b3bc2f9070a2003d2aeb0da73e2c09a7fb
Takumi <nase49946@gmail.com>
2021-10-31 18:19:56 +0900

    タグ修正 #11

--- src/components/ui/CommonHeader.tsx
+++ src/components/ui/CommonHeader.tsx
@@ -6,8 +6,8 @@ export const CommonHeader: VFC = () => {
   return (
     <header className={styles.header}>
       <Link href="/">
-        <a className={styles.header__link} aria-label="トップページへ">
-          BLOG
+        <a aria-label="トップページへ">
+          <h1 className={styles.header__logo}>TECH BLOG</h1>
         </a>
       </Link>
     </header>
