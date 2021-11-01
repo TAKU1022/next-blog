9e09bf758e94f6289e76975f87c87728b6b6942e
Takumi <nase49946@gmail.com>
2021-11-01 22:58:24 +0900

    記事カードコンポーネントスタイル追加 #11

--- src/components/ui/ArticleCard.tsx
+++ src/components/ui/ArticleCard.tsx
@@ -1,6 +1,7 @@
 import { useMemo, VFC } from 'react';
 import Link from 'next/link';
 import { Article, ArticleTag } from '../../types/microCMS';
+import styles from '../../styles/components/ui/ArticleCard.module.scss';
 
 type Props = {
   article: Article;
@@ -14,31 +15,37 @@ export const ArticleCard: VFC<Props> = (props: Props) => {
   );
 
   return (
-    <article>
+    <article className={styles.article}>
       <Link href="/posts/[slug]" as={`/posts/${article.id}`}>
-        <a>
+        <a className={styles.article__imageLink}>
           <img
+            className={styles.article__thumbnail}
             src={article.thumbnail.url}
             alt={`${article.title}のサムネイル`}
+            loading="lazy"
           />
         </a>
       </Link>
-      <p>{date}</p>
-      <Link href="/posts/[slug]" as={`/posts/${article.id}`}>
-        <a>{article.title}</a>
-      </Link>
-      <Link href="/category/[slug]" as={`/category/${article.category.id}`}>
-        <a>{article.category.name}</a>
-      </Link>
-      <ul>
-        {article.tags.map((tag: ArticleTag) => (
-          <li key={tag.id}>
-            <Link href="/tag/[slug]" as={`/tag/${tag.id}`}>
-              <a>{tag.name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <p className={styles.article__date}>{date}</p>
+      <h3 className={styles.article__title}>
+        <Link href="/posts/[slug]" as={`/posts/${article.id}`}>
+          <a>{article.title}</a>
+        </Link>
+      </h3>
+      <div>
+        <Link href="/category/[slug]" as={`/category/${article.category.id}`}>
+          <a className={styles.article__category}>{article.category.name}</a>
+        </Link>
+        <ul className={styles.list}>
+          {article.tags.map((tag: ArticleTag) => (
+            <li className={styles.list__item} key={tag.id}>
+              <Link href="/tag/[slug]" as={`/tag/${tag.id}`}>
+                <a>#{tag.name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </article>
   );
 };
