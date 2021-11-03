ee214b88148adeba2eb7ed68b7d78078baa542cf
Takumi <nase49946@gmail.com>
2021-11-03 12:47:41 +0900

    title追加 #13

--- src/components/page/Posts.tsx
+++ src/components/page/Posts.tsx
@@ -1,4 +1,5 @@
 import { VFC } from 'react';
+import Head from 'next/head';
 import { Article } from '../../types/microCMS';
 import { ArticleCard } from '../ui/ArticleCard';
 import styles from '../../styles/components/page/Posts.module.scss';
@@ -11,19 +12,25 @@ export const Posts: VFC<Props> = (props: Props) => {
   const { articleList } = props;
 
   return (
-    <section className={styles.section}>
-      <div className="container">
-        <h2 className={styles.section__title}>
-          <span className={styles.section__text}>記事一覧</span>
-        </h2>
-        <ul className={styles.list}>
-          {articleList.map((article: Article) => (
-            <li key={article.id}>
-              <ArticleCard article={article} />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </section>
+    <>
+      <Head>
+        <title>記事一覧｜TEHC BLOG</title>
+      </Head>
+
+      <section className={styles.section}>
+        <div className="container">
+          <h2 className={styles.section__title}>
+            <span className={styles.section__text}>記事一覧</span>
+          </h2>
+          <ul className={styles.list}>
+            {articleList.map((article: Article) => (
+              <li key={article.id}>
+                <ArticleCard article={article} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+    </>
   );
 };
