255b6fc86622408ae756383737348af765033c3e
Takumi <nase49946@gmail.com>
2021-11-04 17:27:10 +0900

    記事一覧ページネーション追加 #15

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -6,22 +6,37 @@ import { Article, BlogField } from '../../types/microCMS';
 
 type Props = {
   articleList: Article[];
+  pageCount: number;
+  currentPage: number;
 };
 
 const PostsPage: VFC<Props> = (props: Props) => {
-  const { articleList } = props;
+  const { articleList, pageCount, currentPage } = props;
 
-  return <Posts articleList={articleList} />;
+  return (
+    <Posts
+      articleList={articleList}
+      pageCount={pageCount}
+      currentPage={currentPage}
+    />
+  );
 };
 
 export default PostsPage;
 
 export const getStaticProps: GetStaticProps = async () => {
-  const blogData = await client.get<BlogField>({ endpoint: 'blog' });
+  const perPage = 12;
+  const blogData = await client.get<BlogField>({
+    endpoint: 'blog',
+    queries: { limit: perPage, orders: '-publishedAt' },
+  });
+  const pageCount = Math.ceil(blogData.totalCount / perPage);
 
   return {
     props: {
       articleList: blogData.contents,
+      pageCount,
+      currentPage: 1,
     },
   };
 };
---
+++ src/pages/posts/page/[slug].tsx
@@ -0,0 +1,64 @@
+import { VFC } from 'react';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { Posts } from '../../../components/page/Posts';
+import { client } from '../../../libs/client';
+import { Article, BlogField } from '../../../types/microCMS';
+
+type Props = {
+  articleList: Article[];
+  pageCount: number;
+  currentPage: number;
+};
+
+const PostPagination: VFC<Props> = (props: Props) => {
+  const { articleList, pageCount, currentPage } = props;
+
+  return (
+    <Posts
+      articleList={articleList}
+      pageCount={pageCount}
+      currentPage={currentPage}
+    />
+  );
+};
+
+export default PostPagination;
+
+const perPage = 12;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const currentPage = parseInt(context.params?.slug as string);
+  const blogData = await client.get<BlogField>({
+    endpoint: 'blog',
+    queries: {
+      limit: perPage,
+      offset: (currentPage - 1) * perPage,
+      orders: '-publishedAt',
+    },
+  });
+  const pageCount = Math.ceil(blogData.totalCount / blogData.limit);
+
+  return {
+    props: {
+      articleList: blogData.contents,
+      pageCount,
+      currentPage,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const blogData = await client.get<BlogField>({ endpoint: 'blog' });
+
+  const getPageArray = (start: number, end: number) =>
+    [...Array(end - start + 1)].map((_, i) => start + i);
+
+  const paths = getPageArray(1, Math.ceil(blogData.totalCount / perPage)).map(
+    (number: number) => `/posts/page/${number}`
+  );
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
