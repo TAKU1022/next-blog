f71afe057b159bc57461ac1a2636df7b5317c88c
Takumi <nase49946@gmail.com>
2021-11-06 14:30:51 +0900

    検索ページ作成 #25

---
+++ src/pages/search/[slug].tsx
@@ -0,0 +1,40 @@
+import { VFC } from 'react';
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  PreviewData,
+} from 'next';
+import { ParsedUrlQuery } from 'querystring';
+import { client } from '../../libs/client';
+import { Article, BlogField } from '../../types/microCMS';
+import { Search } from '../../components/page/Search';
+
+type Props = {
+  slug: string;
+  articleList: Article[];
+};
+
+const SearchPage: VFC<Props> = (props: Props) => {
+  const { slug, articleList } = props;
+
+  return <Search slug={slug} articleList={articleList} />;
+};
+
+export default SearchPage;
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>
+) => {
+  const slug = context.params?.slug as string;
+  const blogData = await client.get<BlogField>({
+    endpoint: 'blog',
+    queries: { limit: 1000, q: slug },
+  });
+
+  return {
+    props: {
+      slug,
+      articleList: blogData.contents,
+    },
+  };
+};
